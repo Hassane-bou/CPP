cf21e4dad0304a60a7186021781c0ebbb5d3ea99
Hassane-bou <bouchanehassane25@gmail.com>
2026-04-20 21:04:31 +0100

    last CPP05

--- CPP05/ex03/main.cpp
+++ CPP05/ex03/main.cpp
@@ -12,8 +12,7 @@ int main()
     Intern someRandomIntern;
     AForm* test;
     test = someRandomIntern.makeForm("Robotomy request", "Bender");
-    if(test)
-        delete test;
+    
     }catch(const std::exception& e)
     {
         std::cout << e.what() << '\n';
